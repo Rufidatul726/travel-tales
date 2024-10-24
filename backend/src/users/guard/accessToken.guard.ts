4e75482b62fa74c5260c9c3a806822feb305ab55
MonayemSarker <bsse1228@iit.du.ac.bd>
2024-10-24 11:45:12 +0600

    feat/user module added with authentication

---
+++ backend/src/users/guard/accessToken.guard.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport'
+
+@Injectable()
+export class AccessTokenGuard extends AuthGuard('jwt') {
+  constructor() {
+    // console.log('Executing?');
+    super();
+  }
+}
